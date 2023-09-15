0d102124a85492f8a0b3f04ccf59625acae08cea
Irina <delo_iris@mail.ru>
2023-09-15 12:05:38 +0300

    One

---
+++ client-state/auth/task.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+const signin = document.getElementById('signin');
+const logoutBtn = document.getElementById('logout__btn');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        if(xhr.response.success) {
+            const userId = xhr.response.user_id;
+            localStorage.setItem('userId', userId);
+            logoutBtn.classList.add('logout__btn_active');
+            welcomeDude(userId);
+        } else {
+            alert('Неверный логин или пароль');
+        }
+    }        
+    xhr.send(formData);
+    form.reset();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const userId = localStorage.getItem('userId');
+    if(userId) {
+        welcomeDude(userId);
+    }
+});
+
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('userId');
+    signin.classList.add('signin_active');
+    welcome.classList.remove('welcome_active');
+    logoutBtn.classList.remove('logout__btn_active')    
+})
+
+function welcomeDude(userId) {
+    signin.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+    welcome.querySelector('#user_id').textContent = userId;
+}
---
+++ client-state/text-editor/task.js
@@ -0,0 +1,16 @@
+const storedText = localStorage.getItem('text');
+const textEditor = document.getElementById('editor');
+
+if(storedText !== null) {
+    textEditor.value = storedText;
+}
+
+textEditor.addEventListener('input', () => {
+    localStorage.setItem('text', textEditor.value);
+})
+
+const removeBtn = document.getElementById('remove_btn');
+removeBtn.addEventListener('click', () => {
+    textEditor.value = '';
+    localStorage.removeItem('text');
+})
